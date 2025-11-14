817abf9dbb5c47342ea4f3f15c49372fb7b24ecb
unknown <1554561464@qq.com>
2025-11-14 12:49:31 +0800

    Enjoy Coding

--- src/DB-Bridge/mongodb.js
+++ src/DB-Bridge/mongodb.js
@@ -5,10 +5,22 @@ import schema from './schema.js'
 async function GQuery(para) {
     // para.connectionUrl 连接字
     // para.dbName 数据库名
-    // para.collection 集合名
+    // para.tblname 集合名（表名）
     // para.schema 集合模型（表模型）
     // para.operator 操作符
     // para.query 查询对象
+    // para.sort 排序
+    // para.skip 跳过记录数
+    // para.limit 页记录数
+    // gqueryBody.reference 关联查询
+    /* 语法示例：
+        [
+            {key: "field0", ref_collection: "table0", ref_key: "_id"},
+            {key: "field1", ref_collection: "table1", ref_key: "_id"}
+        ]
+    */
+    // gqueryBody.populate 另一种形式的关联查询（需要用到表模型）
+    // 语法示例：["field0", "field1"]
 
     if(!para.connectionUrl){
         return {code: 1, message: '连接字不存在'}
@@ -18,7 +30,7 @@ async function GQuery(para) {
         return {code: 1, message: '数据库名不存在'}
     }
 
-    if(!para.collection){
+    if(!para.tblname){
         return {code: 1, message: '集合名或表名不存在'}
     }
 
@@ -56,6 +68,29 @@ async function GQuery(para) {
         paraQuery = schema.DTCE({data: para.query, schema: para.schema})
     }
 
+    let paraSort = para.sort ? para.sort : null // 排序
+    let paraSkip = para.skip ? para.skip : 0 // 跳过记录数
+    let paraLimit = para.limit ? para.limit : 0 // 页记录数
+
+    // 关联查询
+    let paraReference = para.reference ? para.reference : null
+    if(para.populate && para.schema){
+        let populate = []
+        para.populate.forEach(i => {
+            if (Object.keys(para.schema).includes(i) && para.schema[i].ref_collection) {
+                populate.push({
+                    key: i,
+                    ref_collection: para.schema[i].ref_collection,
+                    ref_key: para.schema[i].ref_key
+                })
+            }
+        })
+        if(populate.length > 0){
+            paraReference = paraReference ? paraReference : []
+            paraReference = paraReference.concat(populate) // 并入paraReference
+        }
+    }
+
     const client = new MongoClient(para.connectionUrl);
     try {
         // 1. 建立连接
@@ -65,7 +100,7 @@ async function GQuery(para) {
         const database = client.db(para.dbName);
 
         // 3. 选择集合 (Collection)
-        const collection = database.collection(para.collection);
+        const collection = database.collection(para.tblname);
 
         // 4. 执行操作 (例如：插入一条数据)
         const result = await exec({para, collection});
